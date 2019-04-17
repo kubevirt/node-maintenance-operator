11487a22de0e888a2dcadeb59594ab7c5436fcd8
Karel Šimon <ksimon@redhat.com>
2019-04-17 12:47:21 +0200

    changed tree structure of manifests folder

--- build/make-olm.sh
+++ build/make-olm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CSV_VERSION="${1:-0.0.0}"
-BUNDLE_DIR="manifests/olm/bundle"
+BUNDLE_DIR="manifests/node-maintenance-operator/"
+BUNDLE_DIR_VERSION="${BUNDLE_DIR}${CSV_VERSION}/"
 
 
 # TODO: tested with operator-sdk 0.7.0: should we require it?
@@ -15,17 +16,17 @@ which operator-courier &> /dev/null || {
 	exit 2
 }
 
-mkdir -p ${BUNDLE_DIR}
+mkdir -p ${BUNDLE_DIR_VERSION}
 
 operator-sdk olm-catalog gen-csv --csv-version ${CSV_VERSION}
 
 ./build/update-olm.py \
 	deploy/olm-catalog/node-maintenance-operator/${CSV_VERSION}/node-maintenance-operator.v${CSV_VERSION}.clusterserviceversion.yaml > \
-	${BUNDLE_DIR}/node-maintenance-operator.v${CSV_VERSION}.csv.yaml
+	${BUNDLE_DIR_VERSION}/node-maintenance-operator.v${CSV_VERSION}.clusterserviceversion.yaml
 
 # caution: operator-courier (as in 5a4852c) wants *one* entity per yaml file (e.g. it does NOT use safe_load_all)
 for CRD in $( ls deploy/crds/nodemaintenance_*crd.yaml ); do
-	cp ${CRD} ${BUNDLE_DIR}
+	cp ${CRD} ${BUNDLE_DIR_VERSION}
 done
 
 cat << EOF > ${BUNDLE_DIR}/node-maintenance-package.yaml
@@ -36,6 +37,10 @@ channels:
 EOF
 
 echo "built these manifests:"
-ls ${BUNDLE_DIR}
+ls ${BUNDLE_DIR_VERSION}
 
-operator-courier verify ${BUNDLE_DIR} && echo "OLM verify passed" || echo "OLM verify failed"
+cp "${BUNDLE_DIR}node-maintenance-package.yaml" "${BUNDLE_DIR_VERSION}node-maintenance-package.yaml"
+
+operator-courier verify ${BUNDLE_DIR_VERSION} && echo "OLM verify passed" || echo "OLM verify failed"
+
+rm "${BUNDLE_DIR_VERSION}node-maintenance-package.yaml"
