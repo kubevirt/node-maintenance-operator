42b78f948899e30ce1c696578749cb29c01de8a0
Andrew Beekhof <andrew@beekhof.net>
2020-02-27 17:55:33 +1100

    Fix syncing our operator to the cluster
    Signed-off-by: Andrew Beekhof <andrew@beekhof.net>

--- cluster/clean.sh
+++ cluster/clean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash 
 
 echo 'Cleaning up ...'
 
--- cluster/sync.sh
+++ cluster/sync.sh
@@ -4,14 +4,14 @@ TAG="${1:-latest}"
 registry_port=$(./cluster/cli.sh ports registry | tr -d '\r')
 registry=localhost:$registry_port
 
-if [ -d "_out"]; then
+if [ -d "_out" ]; then
     make cluster-clean
 fi
 
 IMAGE_REGISTRY=$registry make container-build-operator container-push-operator
 
 for i in $(seq 1 ${CLUSTER_NUM_NODES}); do
-    ./cluster/cli.sh ssh "node$(printf "%02d" ${i})" "'sudo docker pull registry:5000/node-maintenance-operator:${TAG}'"
+    ./cluster/cli.sh ssh "node$(printf "%02d" ${i})" "sudo docker pull registry:5000/node-maintenance-operator:${TAG}"
     # Temporary until image is updated with provisioner that sets this field
     # This field is required by buildah tool
     ./cluster/cli.sh ssh "node$(printf "%02d" ${i})" 'sudo sysctl -w user.max_user_namespaces=1024'
