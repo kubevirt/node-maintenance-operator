cbe46d1af87dd2f2d66fcf1df28b8d13bb56a24b
Michael Moser <mmoser@redhat.com>
2020-03-04 11:19:04 +0200

    attempt to fix cluster-up to work with openshift.
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- cluster/up.sh
+++ cluster/up.sh
@@ -3,8 +3,8 @@
 KUBERNETES_1_11_IMAGE="k8s-1.11.0@sha256:3412f158ecad53543c9b0aa8468db84dd043f01832a66f0db90327b7dc36a8e8"
 KUBERNETES_1_13_3_IMAGE="k8s-1.13.3@sha256:bc0f02d6b970650eb16d12f97e5aa1376b3a13b0ffed6227db98675be2ca1184"
 OPENSHIFT_IMAGE_3_11="os-3.11.0-crio@sha256:3f11a6f437fcdf2d70de4fcc31e0383656f994d0d05f9a83face114ea7254bc0"
-OPENSHIFT_IMAGE_4_3="okd-4.3@sha256:63abc3884002a615712dfac5f42785be864ea62006892bf8a086ccdbca8b3d38"
-OPENSHIFT_IMAGE="okd-4.1@sha256:e7e3a03bb144eb8c0be4dcd700592934856fb623d51a2b53871d69267ca51c86"
+OPENSHIFT_IMAGE="okd-4.3@sha256:63abc3884002a615712dfac5f42785be864ea62006892bf8a086ccdbca8b3d38"
+OPENSHIFT_IMAGE_4_1="okd-4.1@sha256:e7e3a03bb144eb8c0be4dcd700592934856fb623d51a2b53871d69267ca51c86"
 
 CLUSTER_PROVIDER=${CLUSTER_PROVIDER:-k8s-1.11.0}
 CLUSTER_MEMORY_SIZE=${CLUSTER_MEMORY_SIZE:-5120M}
@@ -55,10 +55,18 @@ elif [[ $image == $OPENSHIFT_IMAGE ]]; then
         CLUSTER_PROVIDER_EXTRA_ARGS="${CLUSTER_PROVIDER_EXTRA_ARGS} --ocp-port 8443"
     fi
 
-    export CLUSTER_MEMORY_SIZE="7680M"
+    #export CLUSTER_MEMORY_SIZE="7680M"
+    #./cluster/cli.sh run --random-ports --reverse --nodes ${CLUSTER_NUM_NODES} --memory ${CLUSTER_MEMORY_SIZE} --background kubevirtci/${image} ${CLUSTER_PROVIDER_EXTRA_ARGS} || onfail
+
 
     # Run OpenShift cluster image
-    ./cluster/cli.sh run --random-ports --reverse --nodes ${CLUSTER_NUM_NODES} --memory ${CLUSTER_MEMORY_SIZE} --background kubevirtci/${image} ${CLUSTER_PROVIDER_EXTRA_ARGS} || onfail
+    ./cluster/cli.sh run okd \
+            --random-ports \
+            --background \
+            --prefix=okd-4.3 \
+            --registry-volume okd-4.3-registry \
+             kubevirtci/${image} \
+             ${CLUSTER_PROVIDER_EXTRA_ARGS} || onfail
 
     ./cluster/cli.sh scp /etc/origin/master/admin.kubeconfig - > ./cluster/.kubeconfig
     ./cluster/cli.sh ssh node01 -- sudo cp /etc/origin/master/admin.kubeconfig ~vagrant/
