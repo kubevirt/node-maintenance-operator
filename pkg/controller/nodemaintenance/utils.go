d874dca4cd0819f73248d235071850645be7461d
Michael Moser <mmoser@redhat.com>
2020-03-11 05:56:36 +0200

    add DeadlineCheck type
    add a type that helps to check if a set deadline has been reached.
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- pkg/controller/nodemaintenance/utils.go
+++ pkg/controller/nodemaintenance/utils.go
@@ -1,6 +1,10 @@
 package nodemaintenance
 
-import corev1 "k8s.io/api/core/v1"
+import (
+    corev1 "k8s.io/api/core/v1"
+    "time"
+    "math"
+)
 
 // ContainsString checks if the string array contains the given string.
 func ContainsString(slice []string, s string) bool {
@@ -30,3 +34,31 @@ func GetPodNameList(pods []corev1.Pod) (result []string) {
 	}
 	return result
 }
+
+type DeadlineCheck struct {
+	isSet    bool
+	deadline time.Time
+}
+
+func NewDeadlineInSeconds(seconds int64) DeadlineCheck {
+	tm := time.Now()
+	tm.Add(time.Duration(seconds) * time.Second)
+
+	return DeadlineCheck{isSet: true, deadline: tm}
+}
+
+func NewDeadlineAt(atTime time.Time) DeadlineCheck {
+	return DeadlineCheck{isSet: true, deadline: atTime}
+}
+
+func (exp DeadlineCheck) IsExpired() bool {
+	return exp.isSet && exp.deadline.After(time.Now())
+}
+func (exp DeadlineCheck) DurationUntilExpiration() time.Duration {
+	if exp.isSet {
+		return exp.deadline.Sub(time.Now())
+	}
+	// better not be here, better check if the deadline has been set.
+	return time.Duration(math.MaxInt64)
+}
+
