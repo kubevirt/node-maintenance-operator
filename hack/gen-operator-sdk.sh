42030d3be82fc087537f5b844aaa2594ea09e290
Michael Moser <mmoser@redhat.com>
2020-04-08 01:38:00 +0300

    Only download operator sdk if needd.
    perform version check when installing operator sdk:
    if the local operator sdk is older than desired then download and run
    the upgrade
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- hack/gen-operator-sdk.sh
+++ hack/gen-operator-sdk.sh
@@ -6,9 +6,48 @@ SELF=$( realpath $0 )
 BASEPATH=$( dirname $SELF )
 TARGET_SDK_VERSION="${1:-v0.16.0}"
 
-DETECTED_SDK_VERSION=$(${BASEPATH}/../operator-sdk version  | awk '{ print $3 }' | sed 's/,$//')
 
-if [[ "$DETECTED_SDK_VERSION" != $TARGET_SDK_VERSION ]]; then
+CURRENT_OPERATOR_SDK=${BASEPATH}/../operator-sdk
+
+if [[ ! -x $CURRENT_OPERATOR_SDK ]]; then
+    set +e
+	CURRENT_OPERATOR_SDK=$(which operator-sdk)
+	set -e
+fi
+
+DETECTED_SDK_VERSION=$($CURRENT_OPERATOR_SDK version  | awk '{ print $3 }' | sed 's/,$//')
+
+
+function check_need_upgrade {
+	local detectedversion="$2"
+	local desiredversion="$1"
+	local needupgrade=0
+	local parseddetectedversion
+	local parseddesiredversion
+
+	if [[ $detectedversion == "" || $desiredversion == "" ]]; then
+		needupgrade=1
+	else
+
+		IFS=$' ' read  -r -a parseddetectedversion <<< $(echo "$detectedversion" | sed -n 's/v\([[:digit:]]*\).\([[:digit:]]*\).\([[:digit:]]*\).*$/\1 \2 \3/p')
+		IFS=$' ' read  -r -a parseddesiredversion <<< $(echo "$desiredversion" | sed -n 's/v\([[:digit:]]*\).\([[:digit:]]*\).\([[:digit:]]*\).*$/\1 \2 \3/p')
+
+		for i in $(seq 1 3); do
+			if [[ ${parseddetectedversion[$i]} -lt ${parseddesiredversion[$i]}  ]]; then
+				needupgrade=1
+				break
+			fi
+		done
+	fi
+	echo "$needupgrade"
+
+}
+
+echo "detected sdk version  $DETECTED_SDK_VERSION"
+
+need_upgrade=$(check_need_upgrade "$TARGET_SDK_VERSION" "$DETECTED_SDK_VERSION")
+
+if [[ $need_upgrade == "1" ]]; then
 
      echo "operator-sdk current version $DETECTED_SDK_VERSION but need $TARGET_SDK_VERSION see https://github.com/operator-framework/operator-sdk)"
      curl -JL https://github.com/operator-framework/operator-sdk/releases/download/${TARGET_SDK_VERSION}/operator-sdk-${TARGET_SDK_VERSION}-x86_64-linux-gnu -o ${BASEPATH}/../operator-sdk
