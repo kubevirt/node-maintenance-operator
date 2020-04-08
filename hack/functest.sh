f736aac6b3126a4f43c35456239dd689afd174dc
Michael Moser <mmoser@redhat.com>
2020-04-08 03:38:14 +0300

    validate openapi schema as part of test
    add openapi schema validation as part of functest.sh
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -15,7 +15,7 @@ if [ -z "$KUBEVIRTCI_CONFIG_PATH" ]; then
     )"
 fi
 
-
+KUBECTL_CMD="${KUBEVIRTCI_PATH}/kubectl.sh"
 
 function new_test() {
     name=$1
@@ -42,4 +42,31 @@ KUBE_CONFIG=$(${KUBEVIRTCI_PATH}/kubeconfig.sh)
 TEST_NAMESPACE=node-maintenance-operator go test ./test/e2e/... -root=$(pwd) -kubeconfig=${KUBE_CONFIG} -globalMan _out/nodemaintenance_crd.yaml --namespacedMan _out/namespace-init.yaml -singleNamespace
 
 echo "E2e tests passed"
+
+echo "check validation of openaAPIV3Schema"
+
+$KUBECTL_CMD create -f _out/namespace-init.yaml
+
+$KUBECTL_CMD create -f _out/nodemaintenance_crd.yaml
+
+echo "validate CRD"
+
+VALIDATE_CRD=$($KUBECTL_CMD get -o yaml crd nodemaintenances.kubevirt.io)
+if [[ $VALIDATE_CRD == "" ]]; then
+	echo "can't validate CRD, check if deployment is running"
+	exit 1
+fi
+
+set +e
+VALIDATION_ERRORS=$(echo "$VALIDATE_CRD" | grep -c "spec.validation.openAPIV3Schema")
+set -e
+
+if [[ $VALIDATION_ERRORS != "0" ]]; then
+	echo "validation of CRD failed"
+	exit 1
+fi
+
+echo "check validation of openaAPIV3Schema passed"
+
+
 
