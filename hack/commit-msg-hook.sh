0331322b138b5cc948b0b0d409a621b46a713b1f
Michael Moser <moser.michael@gmail.com>
2020-06-24 10:57:45 +0300

    whitespace fixes as precommit hook (#91)
    * Signed-off-by: Michael Moser <mmoser@redhat.com>
    use git precommit hooks for detecting whitespace changes
    
    make check - install pre-commit hook for checking whitespaces (instead of
    running whitespace check)
    
    make setupwhitespacehook - install the git pre-commit hook for checking
    whitespaces.
    
    ./hack/precommit-hook.sh - if called with no arguments, it checks whitespace problems on set of files that is about to be commited. if called with setup argument - installs git pre-commit hook for checking of whitespace problems.
    
    make whitespace-check - report error if file has multiple empty lines.
    
    ./hack/whitespace.sh --check-precommit - check whitespace problems on
    files about to be commited (called by pre-commit hook)
    
    also add prepend commit message hook. it adds the signoff message to the
    commit message.
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>
    
    * better checks in git hooks
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>
    
    * review changes
    
    - hook setup: don't force setup on existing hooks
    - append signoff message to existing message
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>
    
    * whitespace check: display suggested changes when issues are found
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>
    
    * don't prepare commit message, as that has legal implications.
    
    just check that signoff message is present and reject if it is not.
    The only way to create commits is now git commit --signoff
    
    Signed-off-by: Michael Moser <mmoser@redhat.com>
    
    Co-authored-by: Michael Moser <mmoser@redhat.com>

---
+++ hack/commit-msg-hook.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TOP_LEVEL=$(git rev-parse --show-toplevel)
+
+if [[ $TOP_LEVEL == "" ]]; then
+  echo "Error: this script must be called from a git repository"
+  exit 1
+fi
+
+if [[ $1 == "setup" ]]; then
+	if [[ ! -f $TOP_LEVEL/.git/hooks/commit-msg ]] && [[ ! -h $TOP_LEVEL/.git/hooks/commit-msg ]]; then
+		ln -s  $TOP_LEVEL/hack/commit-msg-hook.sh $TOP_LEVEL/.git/hooks/commit-msg
+	fi
+	exit 0
+fi
+
+COMMIT_MSG_FILE="$1"
+
+# check if commit message is present
+HAS_SIGNOFF=$(grep -c 'Signed-off-by:' "${COMMIT_MSG_FILE}")
+
+if [[ $HAS_SIGNOFF == "0" ]]; then
+	exec 1>&2
+	cat <<EOF
+Commit Signoff is missing from the commit message.
+Please create the commit with the following commit
+
+	git commit  --signoff
+EOF
+	exit 1
+fi
+
--- hack/whitespace.sh
+++ hack/whitespace.sh
@@ -9,14 +9,41 @@ function fix_one_file() {
 	rm -f ${fname}.tmp
 }
 
+function check_files {
+	while read fname; do
+		IDEAL_FILE=$(sed --follow-symlinks 's/[[:space:]]*$//' ${fname} | sed 'N;/^\n$/D;P;D;')
+		REAL_FILE=$(cat ${fname})
+
+		if [[ "$IDEAL_FILE" != "$REAL_FILE" ]]; then
+			echo "need whitespace fix for file $fname"
+			diff -u <(echo "$REAL_FILE") <(echo "$IDEAL_FILE")
+		fi
+	done
+}
+
 export -f fix_one_file
 
 function fix() {
     git ls-files -- ':!vendor/' | grep -vE '^kubevirtci/' | xargs -I {} bash -c 'fix_one_file "{}"'
 }
 
+function fix_commit() {
+    git diff --cached --name-only | grep -vE '^vendor/' | grep -vE '^kubevirtci/' | xargs -I {} bash -c 'fix_one_file "{}"'
+}
+
 function check() {
-    invalid_files=$(git ls-files -- ':!vendor/' | grep -vE '^kubevirtci/' | xargs egrep -Hn " +$" || true)
+	invalid_files=$(git ls-files -- ':!vendor/' | grep -vE '^kubevirtci/' | check_files)
+
+    if [[ $invalid_files ]]; then
+        echo 'Found trailing whitespaces. Please remove trailing whitespaces using `make fmt`:'
+        echo "$invalid_files"
+        return 1
+    fi
+}
+
+function check_precommit() {
+	invalid_files=$(git diff --cached --name-only | grep -vE '^vendor/' | grep -vE '^kubevirtci/' | check_files)
+
     if [[ $invalid_files ]]; then
         echo 'Found trailing whitespaces. Please remove trailing whitespaces using `make fmt`:'
         echo "$invalid_files"
@@ -26,6 +53,10 @@ function check() {
 
 if [ "$1" == "--fix" ]; then
     fix
+elif [ "$1" == "--fix-commit" ]; then
+    fix_commit
+elif [ "$1" == "--check-precommit" ]; then
+    check_precommit
 else
     check
 fi
