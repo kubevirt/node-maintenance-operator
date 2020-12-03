0c09e1a29d252f2c48074f5c4b959868f4fd9afd
Marc Sluiter <msluiter@redhat.com>
2020-12-03 17:06:50 +0100

    Removed Travis config
    All important jobs were moved to KubeVirtCI, only one left is the one
    for creating github releases, but that's not that important and can be
    migrated in a follow up or done manually if needed
    
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- automation/test.sh
+++ automation/test.sh
@@ -4,6 +4,7 @@ echo "Setup Go paths"
 cd ..
 export GOROOT=/usr/local/go
 export GOPATH=$(pwd)/go
+export GIMME_GO_VERSION=1.13.5
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 mkdir -p $GOPATH
 
@@ -12,15 +13,6 @@ mkdir -p $GOPATH/src/kubevirt.io
 mkdir -p $GOPATH/pkg
 ln -s $(pwd)/node-maintenance-operator $GOPATH/src/kubevirt.io/
 
-pushd $GOPATH/src/kubevirt.io/node-maintenance-operator
-GO_VERSION=$(yq r .travis.yml go) || true
-if [[ $GO_VERSION == "" ]]; then
-	GO_VERSION=$(grep go: .travis.yml  | awk '{ print $2 }' | sed -e 's/"\([^"]*\)"$/\1/')
-fi
-popd
-
-echo "install go version speciifed in travis: ${GO_VERSION}"
-export GIMME_GO_VERSION=${GO_VERSION}
 mkdir -p /gimme
 curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | HOME=/gimme bash >> /etc/profile.d/gimme.sh
 source /etc/profile.d/gimme.sh
