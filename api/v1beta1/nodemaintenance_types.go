acbfffa045a9e40bdaa28a3418df21dc371d78c5
Marc Sluiter <msluiter@redhat.com>
2021-09-15 18:16:58 +0200

    Fill types, generate manifests
    Signed-off-by: Marc Sluiter <msluiter@redhat.com>

--- api/v1beta1/nodemaintenance_types.go
+++ api/v1beta1/nodemaintenance_types.go
@@ -23,23 +23,56 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+const (
+	// NodeMaintenanceFinalizer is a finalizer for a NodeMaintenance CR deletion
+	NodeMaintenanceFinalizer string = "foregroundDeleteNodeMaintenance"
+)
+
+// MaintenancePhase contains the phase of maintenance
+type MaintenancePhase string
+
+const (
+	// MaintenanceRunning - maintenance has started its proccessing
+	MaintenanceRunning MaintenancePhase = "Running"
+	// MaintenanceSucceeded - maintenance has finished succesfuly, cordoned the node and evicted all pods (that could be evicted)
+	MaintenanceSucceeded MaintenancePhase = "Succeeded"
+	// MaintenanceFailed - maintenance has failed
+	MaintenanceFailed MaintenancePhase = "Failed"
+)
+
 // NodeMaintenanceSpec defines the desired state of NodeMaintenance
 type NodeMaintenanceSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of NodeMaintenance. Edit nodemaintenance_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Node name to apply maintanance on/off
+	NodeName string `json:"nodeName"`
+	// Reason for maintanance
+	Reason string `json:"reason,omitempty"`
 }
 
 // NodeMaintenanceStatus defines the observed state of NodeMaintenance
 type NodeMaintenanceStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Phase is the represtation of the maintenance progress (Running,Succeeded,Failed)
+	Phase MaintenancePhase `json:"phase,omitempty"`
+	// LastError represents the latest error if any in the latest reconciliation
+	LastError string `json:"lastError,omitempty"`
+	// PendingPods is a list of pending pods for eviction
+	PendingPods []string `json:"pendingPods,omitempty"`
+	// TotalPods is the total number of all pods on the node from the start
+	TotalPods int `json:"totalpods,omitempty"`
+	// EvictionPods is the total number of pods up for eviction from the start
+	EvictionPods int `json:"evictionPods,omitempty"`
+	// Consecutive number of errors upon obtaining a lease
+	ErrorOnLeaseCount int `json:"errorOnLeaseCount,omitempty"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
 
 // NodeMaintenance is the Schema for the nodemaintenances API
 type NodeMaintenance struct {
